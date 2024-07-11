8b4fe0f06cc37c86b6ffb26b83fa16c62027ff69
Robin Rodricks <robinrodricks7@gmail.com>
2024-07-11 09:43:14 +0530

    minor refactor of RecursionMode

--- FluentStorage/Blobs/ListOptions.cs
+++ FluentStorage/Blobs/ListOptions.cs
@@ -6,21 +6,6 @@ using System.Linq;
 namespace FluentStorage.Blobs {
 
 	/// <summary>
-	/// Controls recursion mode
-	/// </summary>
-	public enum RecursionMode {
-		/// <summary>
-		/// Recurse locally - for each folder on the remote datastore, iterate and query in a separate task
-		/// </summary>
-		Local = 1,
-
-		/// <summary>
-		/// Recurse remotely - let the remote datastore return the entire folder tree
-		/// </summary>
-		Remote
-	}
-	
-	/// <summary>
 	/// Options for listing storage content
 	/// </summary>
 	public class ListOptions {
